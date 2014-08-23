085ea354a4b6ae2d44209c2214fb4c7034e11d85
tonchis <lucas.tolchinsky@gmail.com>
2014-08-22 21:46:20 -0300

    New API. Expectation is not a URL anymore.

--- lib/assert_url.rb
+++ lib/assert_url.rb
@@ -1,29 +1,54 @@
 require "uri"
 
 module AssertUrl
-  PARTS = %W[scheme host port path query]
+  PARTS = %W[scheme host port path query fragment]
 
   PARTS.each do |part|
     error = const_set("#{part.capitalize}Error", Class.new(StandardError))
 
-    define_method(:"assert_#{part}") do |expected, value|
-      expected, value = urify(expected, value).map(&:"#{part}")
+    define_method(:"assert_#{part}_equal") do |expected, value|
+      expected = normalize(part, expected)
+      value = urify(value).send(part.to_sym)
 
       expected == value || (raise error, "expected #{expected}, got #{value}")
     end
   end
 
-  alias assert_hostname assert_host
+  def assert_url_equal(expected, value)
+    expected, value = urify(expected), urify(value)
 
-  def assert_url(expected, value)
-    expected, value = urify(expected, value)
-
-    PARTS.map { |part| send(:"assert_#{part}", expected, value) }.reduce(:&)
+    PARTS.map { |part| send(:"assert_#{part}_equal", expected.send(part.to_sym), value) }.reduce(:&)
   end
 
-  def urify(*args)
-    args.each_with_object([]) { |arg, accum| accum << (arg.kind_of?(URI) ? arg : URI(arg)) }
+  def assert_query_include(expected, value)
+    value = Hash[URI.decode_www_form(urify(value).query)]
+
+    includes?(expected.to_a, value) || (raise QueryError, "expected #{value} to include #{expected}")
+  end
+
+  def includes?(expected, got)
+    expected.all? do |(key, value)|
+      key = key.to_s
+      got.has_key?(key) && got[key] == value
+    end
+  end
+  private :includes?
+
+  def urify(arg)
+    arg.kind_of?(URI) ? arg : URI(arg)
   end
   private :urify
+
+  def normalize(part, expected)
+    case part
+    when "scheme"
+      expected.kind_of?(Symbol) ? expected.to_s : expected
+    when "query"
+      URI.encode_www_form(expected) rescue expected
+    else
+      expected
+    end
+  end
+  private :normalize
 end
 
--- test/assert_url_test.rb
+++ test/assert_url_test.rb
@@ -5,54 +5,70 @@ require_relative "../lib/assert_url"
 include AssertUrl
 
 setup do
-  "http://sub.example.org:8080/path/to/resource?foo=bar"
+  "http://sub.example.org:8080/path/to/resource?foo=bar#fragment"
 end
 
-test "assert_url" do |url|
-  assert assert_url(url, url)
-
-  assert_raise do
-    assert_url("https://example.org", url)
-  end
-end
-
-test "assert_scheme" do |url|
-  assert assert_scheme("http://example.org", url)
+test "assert_scheme_equal" do |url|
+  assert assert_scheme_equal("http", url)
+  assert assert_scheme_equal(:http, url)
 
   assert_raise(AssertUrl::SchemeError) do
-    assert_scheme("ftp://example.org", url)
+    assert_scheme_equal("ftp", url)
   end
 end
 
-test "assert_host" do |url|
-  assert assert_host("https://sub.example.org", url)
+test "assert_host_equal" do |url|
+  assert assert_host_equal("sub.example.org", url)
 
   assert_raise(AssertUrl::HostError) do
-    assert_host("http://example.org", url)
+    assert_host_equal("example.org", url)
   end
 end
 
-test "assert_port" do |url|
-  assert assert_port("http://place.org:8080", url)
+test "assert_port_equal" do |url|
+  assert assert_port_equal(8080, url)
 
   assert_raise(AssertUrl::PortError) do
-    assert_port("ftp://example.org", url)
+    assert_port_equal(80, url)
   end
 end
 
-test "assert_path" do |url|
-  assert assert_path("http://wat.org/path/to/resource", url)
+test "assert_path_equal" do |url|
+  assert assert_path_equal("/path/to/resource", url)
 
   assert_raise(AssertUrl::PathError) do
-    assert_path("ftp://example.org", url)
+    assert_path_equal("/not/the/resource", url)
   end
 end
 
-test "assert_query" do |url|
-  assert assert_query("http://foo.bar/q?foo=bar", url)
+test "assert_query_equal" do |url|
+  assert assert_query_equal({foo: "bar"}, url)
 
   assert_raise(AssertUrl::QueryError) do
-    assert_query("ftp://example.org", url)
+    assert_query_equal("", url)
   end
 end
 
+test "assert_query_include" do
+  assert assert_query_include({foo: "bar"}, "http://example.org/?foo=bar&baz=wat")
+
+  assert_raise(AssertUrl::QueryError) do
+    assert_query_include({foo: "wat"}, "http://example.org/?foo=bar&baz=wat")
+  end
+end
+
+test "assert_fragment_equal" do |url|
+  assert assert_fragment_equal("fragment", url)
+
+  assert_raise(AssertUrl::FragmentError) do
+    assert_fragment_equal("not", url)
+  end
+end
+
+test "assert_url_equal" do |url|
+  assert assert_url_equal(url, url)
+
+  assert_raise do
+    assert_url_equal("https://example.org", url)
+  end
+end
