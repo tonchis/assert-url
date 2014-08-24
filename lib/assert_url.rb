4139262d9f70b05634b69db44f7ebfc0a7f96948
tonchis <lucas.tolchinsky@gmail.com>
2014-08-24 00:57:07 -0300

    Add docs.

--- lib/assert_url.rb
+++ lib/assert_url.rb
@@ -5,30 +5,66 @@ module AssertUrl
     const_set("#{part.capitalize}Error", Class.new(StandardError))
   end
 
+  # @param [String or Symbol] expected The scheme the url should have.
+  # @param [String or URI] value The url you wish to validate.
+  #
+  # @example
+  #
+  #   assert_scheme_equal(:http, "http://example.org")
+  #   assert_scheme_equal("http", "http://example.org")
+  #
   def assert_scheme_equal(expected, value)
     value = urify(value)
 
     expected.to_s == value.scheme || raises(SchemeError, expected, value)
   end
 
+  # @param [String] expected The host the url should have.
+  # @param [String or URI] value The url you wish to validate.
+  #
+  # @example
+  #
+  #   assert_host_equal("example.org", "http://example.org")
+  #
   def assert_host_equal(expected, value)
     value = urify(value)
 
     expected == value.host || raises(HostError, expected, value)
   end
 
+  # @param [Integer] expected The port the url should have.
+  # @param [String or URI] value The url you wish to validate.
+  #
+  # @example
+  #
+  #   assert_port_equal(80, "http://example.org")
+  #
   def assert_port_equal(expected, value)
     value = urify(value)
 
     expected == value.port || raises(PortError, expected, value)
   end
 
+  # @param [String] expected The path the url should have.
+  # @param [String or URI] value The url you wish to validate.
+  #
+  # @example
+  #
+  #   assert_path_equal("/path", "http://example.org/path")
+  #
   def assert_path_equal(expected, value)
     value = urify(value)
 
     expected == value.path || raises(PathError, expected, value)
   end
 
+  # @param [Hash] expected The query the url should have.
+  # @param [String or URI] value The url you wish to validate.
+  #
+  # @example
+  #
+  #   assert_query_equal({foo: "bar"}, "http://example.org/?foo=bar")
+  #
   def assert_query_equal(expected, value)
     value = urify(value)
     expected = (URI.encode_www_form(expected) rescue expected)
@@ -36,18 +72,41 @@ module AssertUrl
     expected == value.query || raises(QueryError, expected, value)
   end
 
+  # @param [String] expected The fragment the url should have.
+  # @param [String or URI] value The url you wish to validate.
+  #
+  # @example
+  #
+  #   assert_fragment_equal("fragment", "http://example.org/path#fragment")
+  #
   def assert_fragment_equal(expected, value)
     value = urify(value)
 
     expected == value.fragment || raises(FragmentError, expected, value)
   end
 
+  # {#assert_url_equal} runs all the validations above. It's not a String comparison.
+  #
+  # @param [String or URI] expected The url you wish to have.
+  # @param [String or URI] value The url you wish to validate.
+  #
+  # @example
+  #
+  #   assert_url_equal(URI("http://example.org"), "http://example.org")
+  #
   def assert_url_equal(expected, value)
     expected, value = urify(expected), urify(value)
 
     PARTS.map { |part| send(:"assert_#{part}_equal", expected.send(part.to_sym), value) }.reduce(:&)
   end
 
+  # @param [String or Symbol] expected The key-value pairs that the url must include.
+  # @param [String or URI] value The url you wish to validate.
+  #
+  # @example
+  #
+  #   assert_query_include({foo: "bar"}, "http://example.org/?foo=bar&baz=wat")
+  #
   def assert_query_include(expected, value)
     value = Hash[URI.decode_www_form(urify(value).query)]
 
